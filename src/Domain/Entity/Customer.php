30170af12129596be386f3035d57323488b317e9
essam saber <essam@badrsystems.com>
2019-01-29 23:50:04 +0200

    The Domain Layer

---
+++ src/Domain/Entity/Customer.php
@@ -0,0 +1,30 @@
+<?php 
+namespace CleanPhp\Invoicer\Domain\Entity;
+
+class Customer extends AbstractEntity
+{
+	protected $name;
+	protected $email;
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function setName($name)
+	{
+		$this->name = $name;
+		return $this;
+	}
+
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+	public function setEamil($email)
+	{
+		$this->email = $email;
+		return $this;
+	}
+}
